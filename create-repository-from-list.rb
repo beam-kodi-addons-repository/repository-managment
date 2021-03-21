4c68ccefb3ffb6cfeb8b50222ddd0370306f922b
Ondrej Bima <ondra@bimovi.cz>
2021-03-21 20:05:51 +0100

    deployment podle kodi verze

--- create-repository-from-list.rb
+++ create-repository-from-list.rb
@@ -6,6 +6,7 @@ require 'bundler/setup'
 require 'optparse'
 
 require_relative 'tools'
+require 'fileutils'
 require 'rexml/document'
 
 GITHUB_USER = ENV['GITHUB_USER']
@@ -30,71 +31,79 @@ abort "Missing repository name where is package list stored, use -n to spefify" 
 
 $logger.info("Fetching current package list")
 git_repo = clone_github_repo(options[:package_list_user], options[:package_list_repository], "master", "cloned-current-package-list")
-package_file_path = "cloned-current-package-list/packages.yml"
-$logger.info("Loading package list")
-packages_content = load_package_list(package_file_path)
+all_packages_content = Hash.new
+
+Dir.glob("cloned-current-package-list/packages-*.yml").each { |package_file_path| 
+  kodi_version = File.basename(package_file_path).match(/packages-(?<kodi-version>.+).yml/)["kodi-version"].downcase
+  $logger.info("Loading package list for #{kodi_version}")
+  all_packages_content[kodi_version] = load_package_list(package_file_path)
+}
+
 $logger.info("Search last commit message")
 last_commit = get_last_commit_message(git_repo)
 $logger.info("Destroying packages list repository")
 destroy_git_repo(git_repo)
 
-$logger.info("Fetching current packare repository")
+$logger.info("Fetching current package repository")
 cloned_packages_path = "cloned-current-package-repository"
 git_repo = clone_github_repo(options[:package_list_user], options[:package_list_repository], "gh-pages", cloned_packages_path)
 
-xmldoc = REXML::Document.new
-xmldoc << REXML::XMLDecl.new("1.0", "UTF-8", "yes")
-xmldoc_addons = xmldoc.add_element("addons")
+all_packages_content.each { |kodi_version, packages_content| 
 
-packages_content.each_pair { |pkg_id, pkg_info|
+  xmldoc = REXML::Document.new
+  xmldoc << REXML::XMLDecl.new("1.0", "UTF-8", "yes")
+  xmldoc_addons = xmldoc.add_element("addons")
 
-  $logger.info("Processing package #{pkg_info[:addon_id]}")
-  package_home_dir = "#{cloned_packages_path}/packages/#{pkg_info[:addon_id]}"
-  package_dest_path = "#{package_home_dir}/#{pkg_info[:package_file]}"
+  packages_content.each_pair { |pkg_id, pkg_info|
 
-  if pkg_info[:type] == "gh-repository"
-    package_hash_match = (File.exists?(package_dest_path) && pkg_info[:sha256] == get_sha256_from_zip(package_dest_path))
-  else
-    package_hash_match = (File.exists?(package_dest_path) && pkg_info[:sha256] == sha256_file(package_dest_path))
-  end
+    $logger.info("Processing package #{pkg_info[:addon_id]} @ #{kodi_version}")
+    package_home_dir = "#{cloned_packages_path}/#{kodi_version}/packages/#{pkg_info[:addon_id]}"
+    package_dest_path = "#{package_home_dir}/#{pkg_info[:package_file]}"
 
-  if package_hash_match
-    $logger.info("Package #{pkg_info[:addon_id]} already fetched")
-  else
-    $logger.info("Fetching package #{pkg_info[:addon_id]}")
-    Dir.mkdir(package_home_dir) unless File.directory?(package_home_dir)
-    fetch_package(pkg_info, package_dest_path, pkg_info[:addon_id])
-  end
-
-  # add addon xml into addons
-  addon_xml = get_file_content_from_zip(package_dest_path, "addon.xml").force_encoding("utf-8")
-  addon_element = get_addon_xml_element(addon_xml)
-  xmldoc_addons.add_element(addon_element)
-
-  # iages handle
-  [ "icon.png", "fanart.jpg" ].each { |image_path|
-    File.delete("#{package_home_dir}/#{image_path}") if File.exists?("#{package_home_dir}/#{image_path}")
-  }
-  [["icon.png", "icon.png"], ["icon.png","resources/icon.png"], ["fanart.jpg","fanart.jpg"], ["fanart.jpg","resources/fanart.jpg"]].each { |image_path|
-    file_content = get_file_content_from_zip(package_dest_path, image_path.last)
-    if file_content
-      File.open("#{package_home_dir}/#{image_path.first}","wb") { |f| f.write(file_content) }
-      next
+    if pkg_info[:type] == "gh-repository"
+      package_hash_match = (File.exists?(package_dest_path) && pkg_info[:sha256] == get_sha256_from_zip(package_dest_path))
+    else
+      package_hash_match = (File.exists?(package_dest_path) && pkg_info[:sha256] == sha256_file(package_dest_path))
     end
+
+    if package_hash_match
+      $logger.info("Package #{pkg_info[:addon_id]} @ #{kodi_version} already fetched")
+    else
+      $logger.info("Fetching package #{pkg_info[:addon_id]} @ #{kodi_version}")
+      FileUtils.mkdir_p(package_home_dir) unless File.directory?(package_home_dir)
+      fetch_package(pkg_info, package_dest_path, pkg_info[:addon_id])
+    end
+
+    # add addon xml into addons
+    addon_xml = get_file_content_from_zip(package_dest_path, "addon.xml").force_encoding("utf-8")
+    addon_element = get_addon_xml_element(addon_xml)
+    xmldoc_addons.add_element(addon_element)
+
+    # iages handle
+    [ "icon.png", "fanart.jpg" ].each { |image_path|
+      File.delete("#{package_home_dir}/#{image_path}") if File.exists?("#{package_home_dir}/#{image_path}")
+    }
+    [["icon.png", "icon.png"], ["icon.png","resources/icon.png"], ["fanart.jpg","fanart.jpg"], ["fanart.jpg","resources/fanart.jpg"]].each { |image_path|
+      file_content = get_file_content_from_zip(package_dest_path, image_path.last)
+      if file_content
+        File.open("#{package_home_dir}/#{image_path.first}","wb") { |f| f.write(file_content) }
+        next
+      end
+    }
   }
+
+  $logger.info("Saving #{kodi_version}/addons.xml")
+  formatter = REXML::Formatters::Pretty.new(3)
+  formatter.compact = true
+  output = File.new("#{cloned_packages_path}/#{kodi_version}/addons.xml","w")
+  formatter.write(xmldoc, output)
+  output.close
+
+  $logger.info("Saving #{kodi_version}/addons.xml.md5")
+  File.open("#{cloned_packages_path}/#{kodi_version}/addons.xml.md5","w") { |f| f.write(md5_file("#{cloned_packages_path}/#{kodi_version}/addons.xml")) }
 }
 
-$logger.info("Saving addons.xml")
-formatter = REXML::Formatters::Pretty.new(3)
-formatter.compact = true
-output = File.new("#{cloned_packages_path}/addons.xml","w")
-formatter.write(xmldoc, output)
-output.close
-
-$logger.info("Saving addons.xml.md5")
-File.open("#{cloned_packages_path}/addons.xml.md5","w") { |f| f.write(md5_file("#{cloned_packages_path}/addons.xml")) }
-
-if packages_content.count == 0
+if all_packages_content.inject(0) { |total, (kodi_version, packages)| total += packages.count } == 0
   $logger.info("No packages for repository")
 elsif repo_has_been_changed?(git_repo)
   $logger.info("Repository has been changed, committing")
